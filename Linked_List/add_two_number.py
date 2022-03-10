f9d4e5385b406e3608a133e95a09b3a8c0a2e0ec
Vadlamudi Naveen <39674418+VadlamudiNaveen@users.noreply.github.com>
2022-03-10 09:29:45 +0530

    day-4

--- Linked_List/add_two_number.py
+++ Linked_List/add_two_number.py
@@ -1,4 +1,6 @@
 '''
+Source- Leetcode 
+
 You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
 
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
@@ -17,11 +19,8 @@ Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
 Output: [8,9,9,9,0,0,0,1]
 
 T4:
-[2,4,9]
-[5,6,4,9]
-output:[7,0,8]
-[7,0,4,0,1]
-[8,9,9,9,0,0,0,1]
+Input:l1 = [2,4,9], l2 = [5,6,4,9]
+output: [7,0,4,0,1]
 
 
 '''
