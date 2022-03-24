8bdcba0b6cdcff22e77c72e9e2998dfef5214e95
Vadlamudi Naveen <39674418+VadlamudiNaveen@users.noreply.github.com>
2022-03-24 23:53:36 +0530

    Strings-D8

---
+++ Programming_Skills_Study_Plan/String_D_8/march_24_2022_Goal_Parser.py
@@ -0,0 +1,57 @@
+'''
+You own a Goal Parser that can interpret a string command. The command consists of an alphabet of "G", "()" and/or "(al)" in some order. The Goal Parser will interpret "G" as the string "G", "()" as the string "o", and "(al)" as the string "al". The interpreted strings are then concatenated in the original order.
+
+Given the string command, return the Goal Parser's interpretation of command.
+
+ 
+
+Example 1:
+
+Input: command = "G()(al)"
+Output: "Goal"
+Explanation: The Goal Parser interprets the command as follows:
+G -> G
+() -> o
+(al) -> al
+The final concatenated result is "Goal".
+Example 2:
+
+Input: command = "G()()()()(al)"
+Output: "Gooooal"
+Example 3:
+
+Input: command = "(al)G(al)()()G"
+Output: "alGalooG"
+ 
+
+Constraints:
+
+1 <= command.length <= 100
+command consists of "G", "()", and/or "(al)" in some order.
+'''
+class Solution:
+    def interpret(self, command: str) -> str:
+        stk = []
+        res = ""
+        for ind in range(len(command)):
+            
+                    # if the parathesis closes 
+                
+                    if command[ind] == ')':
+                        if len(stk) > 0 and stk[-1] == '(':
+                                # if the n-2 index value is character then pop the value from stk
+                                if ord(command[ind-1]) >= 97 and ord(command[ind-1]) <= 122:
+                                        stk.pop()
+                                # if n-2 index value is not a character then
+                                else:
+                                        res += 'o'
+                                        stk.pop()
+                                        
+                    # if it is opened then push to stack
+                    
+                    elif command[ind] == '(':
+                            stk.append(command[ind])
+                    else:
+                            res += command[ind]
+        return res
+                                   
